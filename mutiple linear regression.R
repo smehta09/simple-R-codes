07238c21790d44922ff5bc51440eb222c3b3417b
smehta09 <40140194+smehta09@users.noreply.github.com>
2018-06-10 20:19:21 +0530

    Add files via upload

---
+++ mutiple linear regression.R
@@ -0,0 +1,33 @@
+#linear regression
+print("data for speed")
+
+x<-c(4,4,7,7,8,9,10,10,10,10,11)
+
+print("data of distance")
+
+y<-c(2,10,4,22,16,10,18,26,34,17)
+
+print("relationship model") 
+relation<-lm(dist~speed, data=cars) 
+relation
+
+
+print(relation)
+
+
+m<-summary(relation)$coefficients
+
+
+
+m[1,1]
+
+m<-coefficients(relation) m<-relation$coefficients
+
+
+str(m)
+
+a<-data.frame(speed=55)
+print(a)
+result<-predict(relation,a)
+result
+plot(cars$speed,cars$dist)
