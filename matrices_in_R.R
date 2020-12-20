1b5113eca09eb57d6748f6057fa0d18f51aec641
Simran Mehta <40140194+smehta09@users.noreply.github.com>
2020-12-20 21:49:50 +0530

    Add files via upload
    Matrices in R

---
+++ matrices_in_R.R
@@ -0,0 +1,19 @@
+#creating matrices in R
+data1 <- 1:6
+data2 <- 10:15
+data1+data2
+data1-data2
+data1*data2
+p <- matrix(0,2,2)
+p
+p <- matrix(c(1,2,3,4),2,2)
+p
+p <- matrix(c(1,2,3,4),3,3)
+p
+p[1,1]=10
+p
+p*p
+p
+
+x0 <- c(0,0,1,0,0) 
+
